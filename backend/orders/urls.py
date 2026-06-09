3234a3f3927de83005e56eb148f6c6c6fa45b78e
yakshasri11 <yakshasri9@gmail.com>
2026-06-09 23:02:02 +0530

    Initial commit

--- backend/orders/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',          views.order_list_create, name='order-list'),
-    path('<int:pk>/', views.order_detail,       name='order-detail'),
-]
--- backend/shopnest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/products/', include('products.urls')),
-    path('api/users/',    include('users.urls')),
-    path('api/orders/',   include('orders.urls')),
-]
