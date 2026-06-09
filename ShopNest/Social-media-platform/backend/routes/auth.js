47bbfa238374eed0b7ee3f4a391cb0c1bdc54368
yakshasri11 <yakshasri9@gmail.com>
2026-06-09 23:00:26 +0530

    Remove messy ShopNest folder

--- ShopNest/Social-media-platform/backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const r = require('express').Router();
-const { authenticate } = require('../middleware/auth');
-const c = require('../controllers/authController');
-r.post('/register', c.register);
-r.post('/login', c.login);
-r.get('/me', authenticate, c.me);
-r.post('/logout', c.logout);
-module.exports = r;
